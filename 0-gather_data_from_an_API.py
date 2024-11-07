44750c8c60f61202ab9300d575603350ab9efe9b
chepeniv <jose.olmos@atlasschool.com>
2024-11-07 06:18:35 -0600

    exploring and laying down the manipulation logic for Request object

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -2,26 +2,25 @@
 '''
 python script that uses a provided rest api to fetch an employee by id given
 and returns information about their todo list progress
-
-- accept an integer (employee id) as a param
-- display the employee todo list progress in this format :
-----
-Employee EMPLOYEE_NAME is done with
-task(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-    TASK_TITLE
-----
 '''
 
+from json.decoder import JSONDecoder
 from sys import argv
 import requests
-# import urllib
 
 
 # https://jsonplaceholder.typicode.com
-# routes: /todos, /todos/<idnum>
+# routes: /todos, /todos/<idnum>, /users, /users/<idnum>
 # api: GET, POST, PUT, PATCH, DELETE
 def process_request():
     '''
+    takes an integer representing a employee id from argv and display the found
+    employee's todo list progress in the following format :
+    ----
+    Employee EMPLOYEE_NAME is done with
+    task(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+        TASK_TITLE
+    ----
     '''
     if len(argv) < 2:
         print("no employee id provided")
@@ -33,6 +32,21 @@ def process_request():
     except ValueError:
         print("invalid employee id provided")
         return
+
+    json_decoder = JSONDecoder()
+
+    employee_get = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{given_id}")
+
+    if employee_get.status_code == 200:
+        todos_get = requests.get(
+                "https://jsonplaceholder.typicode.com/todos")
+        todos_string = todos_get.content.decode()
+        todos_dict = json_decoder.decode(todos_string)
+        for item in todos_dict:
+            print(f"{item}")
+    else:
+        print("employee not found")
 
 
 if __name__ == "__main__":
